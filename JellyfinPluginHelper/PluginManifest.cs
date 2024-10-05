2cf4c96d1340d02c19373d999e69f1360de62f93
hexxone <dominics.pc@gmail.com>
2024-10-05 18:24:08 +0200

    fix: PluginHelper

---
+++ JellyfinPluginHelper/PluginManifest.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace JellyfinPluginHelper;
+
+public struct PluginManifest
+{
+    [JsonPropertyName("guid")] public string Guid { get; set; }
+
+    [JsonPropertyName("name")] public string Name { get; set; }
+
+    [JsonPropertyName("overview")] public string Overview { get; set; }
+
+    [JsonPropertyName("description")] public string Description { get; set; }
+
+    [JsonPropertyName("owner")] public string Owner { get; set; }
+
+    [JsonPropertyName("category")] public string Category { get; set; }
+
+    [JsonPropertyName("imageUrl")] public string ImageUrl { get; set; }
+
+    [JsonPropertyName("versions")] public List<PluginVersion>? Versions { get; set; }
+}
